1ecb6a1c335bb0ff707d0f1d68efecae6886ce81
Chinmay Chandak <chandakchinmay@gmail.com>
2018-08-28 00:08:24 +0530

    gravity gun done!
    done some hacky stuff:
    set the position of box to player fixed position in every update! :p
    extraced contents of Cmds to another function..
    this functions are called with the respective Cmds
    
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

--- Assets/Scripts/PlayerGunController.cs
+++ Assets/Scripts/PlayerGunController.cs
@@ -4,26 +4,25 @@ using UnityEngine.Networking;
 public class PlayerGunController : NetworkBehaviour
 {
     [System.Serializable]
-    public class Box
+    public class Player
+    {
+        public Transform spawnPoint;
+        public GameObject sp;
+        public float gravityGunRange = 10;
+        public Camera camera;
+    }
+
+    public struct Box
     {
         public GameObject obj;
         public Rigidbody rigidbody;
     }
 
-    [System.Serializable]
-    public class Bullet
+    public struct Bullet
     {
         public GameObject obj;
         public PortalBulletController controller;
         public Rigidbody rigidbody;
-    }
-
-    [System.Serializable]
-    public class Player
-    {
-        public Transform spawnPoint;
-        public float gravityGunRange = 10;
-        public Camera camera;
     }
 
     public Player player;
@@ -36,7 +35,9 @@ public class PlayerGunController : NetworkBehaviour
 
     private void Start()
     {
-        bullet.obj = Instantiate(bulletPrefab);
+        bullet.obj = Instantiate(bulletPrefab, player.spawnPoint);
+        bullet.obj.transform.parent = null;
+        bullet.obj.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
         bullet.obj.SetActive(false);
         bullet.rigidbody = bullet.obj.GetComponent<Rigidbody>();
         bullet.controller = bullet.obj.GetComponent<PortalBulletController>();
@@ -47,40 +48,53 @@ public class PlayerGunController : NetworkBehaviour
         if (!isLocalPlayer)
             return;
 
-        if (Input.GetMouseButtonDown(1))
-        {
-            //Debug.Log("fire in the hole");
-            if (isHoldingBox)
-            {
-                CmdDropBox();
-            }
-            else
-            {
-                CmdGravityGun();
-            }
-        }
-
         if (Input.GetMouseButtonDown(0))
         {
             if (!isHoldingBox)
             {
-
+                //TODO: convert this to proper Server-Client network code
                 bullet.obj.SetActive(false);
                 bullet.rigidbody.isKinematic = true;
                 bullet.obj.transform.position = player.spawnPoint.position;
                 bullet.controller.forward = player.camera.transform.forward;
-                bullet.rigidbody.isKinematic = true;
+                bullet.controller.ResetObj();
+                bullet.rigidbody.isKinematic = false;
                 bullet.obj.SetActive(true);
             }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (isHoldingBox)
+            {
+                ThrowBox();
+                CmdThrowBox();
+            }
+            else
+            {
+                GravityGun(player.spawnPoint.position, player.spawnPoint.forward, player.camera.transform.forward);
+                CmdGravityGun(player.spawnPoint.position, player.spawnPoint.forward, player.camera.transform.forward);
+            }
+        }
+
+        if(box.obj != null)
+        {
+            box.obj.transform.position = player.spawnPoint.position + player.spawnPoint.forward * 0.2f - Vector3.up * 0.3f;
+            //box.obj.transform.rotation = Quaternion.identity;
         }
     }
 
     [Command]
-    private void CmdGravityGun()
+    private void CmdGravityGun(Vector3 _position, Vector3 _forward, Vector3 _direction)
+    {
+        GravityGun(_position, _forward, _direction);
+    }
+
+    private void GravityGun(Vector3 _position, Vector3 _forward, Vector3 _direction)
     {
         RaycastHit hit;
 
-        if (Physics.Raycast(player.spawnPoint.position, player.camera.transform.forward, out hit, player.gravityGunRange))
+        if (Physics.Raycast(_position, _direction, out hit, player.gravityGunRange))
         {
             string goTag = hit.transform.gameObject.tag;
             Debug.Log(goTag);
@@ -91,22 +105,26 @@ public class PlayerGunController : NetworkBehaviour
                 //get the box
                 box.obj = hit.transform.gameObject;
                 box.rigidbody = box.obj.GetComponent<Rigidbody>();
-
                 box.obj.transform.parent = player.spawnPoint.parent;
                 box.rigidbody.useGravity = false;
                 box.rigidbody.mass = 0.001f;
                 //boxRb.position = spawnPoint.position;
                 box.rigidbody.isKinematic = true;
-                box.obj.transform.position = player.spawnPoint.position - player.spawnPoint.forward * 0.2f;
+                box.obj.transform.position = _position + _forward * 0.2f - Vector3.up * 0.3f;
                 box.obj.transform.localScale = new Vector3(2, 2, 2);
-
+                Debug.Log(box);
                 isHoldingBox = true;
             }
         }
     }
 
     [Command]
-    private void CmdDropBox()
+    private void CmdThrowBox()
+    {
+        ThrowBox();
+    }
+
+    private void ThrowBox()
     {
         //reset the rigidbody component of box
         box.obj.transform.parent = null;
@@ -114,6 +132,8 @@ public class PlayerGunController : NetworkBehaviour
         box.rigidbody.isKinematic = false;
         box.rigidbody.useGravity = true;
         box.rigidbody.mass = 10000;
+
+        box.obj = null;
 
         isHoldingBox = false;
     }
--- Assets/Scripts/PortalBulletController.cs
+++ Assets/Scripts/PortalBulletController.cs
@@ -14,10 +14,15 @@ public class PortalBulletController : MonoBehaviour
 		rb.velocity = forward * speedMultiplier;
 	}
 
-//    void FixedUpdate()
-//    {
-//        rb.AddForce(forward, ForceMode.Impulse);
-//    }
+    public void ResetObj()
+    {
+        rb.velocity = forward * speedMultiplier;
+    }
+
+    //    void FixedUpdate()
+    //    {
+    //        rb.AddForce(forward, ForceMode.Impulse);
+    //    }
 
     void OnCollisionEnter(Collision other)
     {
@@ -27,6 +32,13 @@ public class PortalBulletController : MonoBehaviour
         Quaternion portalRotation = Quaternion.LookRotation(contactNormal, Vector3.up);
         GameObject spawnedPortal = Instantiate(portalPrefab, collisionPoint - forward.normalized * 1.01f, portalRotation);
 
-        Destroy(this.gameObject);
+        //Destroy(this.gameObject);
+        //instead of destroying.. disable it..
+        //TODO: same as PlayerGunController
+        //make a permanant referance to portal
+        //disable already created one 
+        //and respwan it in new position
+        //do the same in the manager script for Portal 
+        this.gameObject.SetActive(false);
     }
 }
