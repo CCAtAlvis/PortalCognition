67d4012958e0a79a107abd3ea4ba1ed41deaa86e
Chinmay Chandak <chandakchinmay@gmail.com>
2018-08-28 01:17:29 +0530

    MultiTrigger done!
    built similar to previous Trigger system
    MultiTrigger also have AlwaysOpen property
    so we have 2 types main trigger
    
    but now using MonoBehaviour instead of NetworkBehaviour
    as its not actually necessory to sync these over the network
    and enabling or disbling objects is not that expensive
    
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

---
+++ Assets/Scripts/MultiTrigger.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class MultiTrigger : MonoBehaviour
+{
+    public int noOfTriggers = 0;
+    public GameObject triggerObject;
+    public bool alwaysOpen = true;
+
+    private int activatedTriggers = 0;
+    private bool firstTime = true;
+
+    public void IncreaseTriggerCount()
+    {
+        //if (!isServer)
+        //    return;
+
+        activatedTriggers++;
+        CheckTriggerCount();
+    }
+
+    public void DeceaseTriggerCount()
+    {
+        //if (!isServer)
+        //    return;
+
+        activatedTriggers--;
+        CheckTriggerCount();
+    }
+
+    private void CheckTriggerCount()
+    {
+        Debug.Log(activatedTriggers + " : " + noOfTriggers);
+        if (activatedTriggers == noOfTriggers && firstTime)
+        {
+            Debug.Log("in here");
+            //RpcSetTrigger(false);
+            triggerObject.SetActive(false);
+            firstTime = false;
+        }
+        else if (activatedTriggers < noOfTriggers)
+        {
+            if (!alwaysOpen)
+            {
+                //RpcSetTrigger(true);
+                triggerObject.SetActive(true);
+            }
+        }
+    }
+
+    //[ClientRpc]
+    //private void RpcSetTrigger(bool _set)
+    //{
+    //    //triggerObject.SetActive(triggerObject.activeSelf ^ true);
+    //    triggerObject.SetActive(_set);
+    //}
+}
---
+++ Assets/Scripts/MultiTriggerPoint.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class MultiTriggerPoint : MonoBehaviour
+{
+    public MultiTrigger trigger;
+    private bool isTriggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (!isServer)
+        //    return;
+
+        Debug.Log(other.tag);
+
+        if ("Box" != other.tag && "Player" != other.tag)
+            return;
+
+        //if ("Box" == other.tag)
+        //{
+        //    Debug.Log("box entered the trigger.");
+        //}
+
+        //if ("Player" == other.tag)
+        //{
+        //    Debug.Log("player entered the trigger.");
+        //}
+
+        Debug.Log(isTriggered);
+        if (!isTriggered)
+        {
+            trigger.IncreaseTriggerCount();
+            Debug.Log("++ trigger count");
+        }
+
+        isTriggered = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //if (!isServer)
+        //    return;
+
+        Debug.LogError("something exited");
+
+        trigger.DeceaseTriggerCount();
+        isTriggered = false;
+    }
+}
