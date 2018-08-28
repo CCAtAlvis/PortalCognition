abe286f5f519f89d34d208b408829e5238132886
Chinmay Chandak <chandakchinmay@gmail.com>
2018-08-28 23:34:04 +0530

    All props till now test on multiplayer
    everyting working properly!
    also now player dies when it collides with LaserBeamEnemy
    
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

--- Assets/Scripts/LaserBeamEnemy.cs
+++ Assets/Scripts/LaserBeamEnemy.cs
@@ -4,6 +4,7 @@ public class LaserBeamEnemy : MonoBehaviour
 {
 	public float toggleTime;
 	public GameObject beams;
+    public BoxCollider trigger;
 
 	private float timer;
 
@@ -14,16 +15,18 @@ public class LaserBeamEnemy : MonoBehaviour
 		if (timer > toggleTime)
 		{
 			beams.SetActive (beams.activeSelf ^ true);
+            trigger.enabled = trigger.enabled ^ true;
 			timer = 0;
 		}
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if ("Box" == other.tag || "Player" == other.tag)
+		if (/*"Box" == other.tag ||*/ "Player" == other.tag)
 		{
-			//TODO: make a script Destroy or ResetObject
-			//so all Destroy functions can be accessed easily..
+            //TODO: make a script Destroy or ResetObject
+            //so all Destroy functions can be accessed easily..
+            other.GetComponent<PlayerMovementController>().Destroy();
 		}
 	}
 }
