0d449a245aaa5cd11891761099a42bf1ea5e9ce1
Chinmay Chandak <chandakchinmay@gmail.com>
2018-08-28 19:52:17 +0530

    laser beam.. atleast for single player
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

---
+++ Assets/Scripts/LaserBeamEnemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class LaserBeamEnemy : MonoBehaviour
+{
+	public float toggleTime;
+	public GameObject beams;
+
+	private float timer;
+
+	void Update ()
+	{
+		timer += Time.deltaTime;
+
+		if (timer > toggleTime)
+		{
+			beams.SetActive (beams.activeSelf ^ true);
+			timer = 0;
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if ("Box" == other.tag || "Player" == other.tag)
+		{
+			//TODO: make a script Destroy or ResetObject
+			//so all Destroy functions can be accessed easily..
+		}
+	}
+}
