265898dd13d78d0f371cd32ab2ea1e22c1bfcd8d
Chinmay Chandak <chandakchinmay@gmail.com>
2018-08-30 18:20:22 +0530

    Jump trigger done!
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

---
+++ Assets/Scripts/JumpTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class JumpTrigger : MonoBehaviour
+{
+	public float velocity;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		GameObject other = collision.collider.gameObject;
+		if ("Player" == other.tag)
+		{
+			other.GetComponent<Rigidbody> ().velocity += transform.forward * velocity;
+		}
+	}
+}
