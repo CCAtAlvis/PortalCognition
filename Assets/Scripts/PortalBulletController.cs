d2542635d8109304757239504b81f6014b1e8781
Janhavi <janhavi.dubule16@siesgst.ac.in>
2018-08-31 08:41:27 +0530

    Portal Mechanism added. Rotating tiles added.

--- Assets/Scripts/PortalBulletController.cs
+++ Assets/Scripts/PortalBulletController.cs
@@ -2,6 +2,7 @@
 
 public class PortalBulletController : MonoBehaviour
 {
+    public GameObject otherPortal;
     public GameObject portalPrefab;
     public Vector3 forward;
 	public int speedMultiplier = 6;
@@ -9,6 +10,11 @@ public class PortalBulletController : MonoBehaviour
     [SerializeField]
     private Rigidbody rb;
 
+    /*public void type(GameObject go)
+    {
+        Debug.Log("type set in pbc");
+        portalPrefab = go;
+    }*/
 	private void Start()
 	{
 		rb.velocity = forward * speedMultiplier;
@@ -30,8 +36,12 @@ public class PortalBulletController : MonoBehaviour
         Vector3 contactNormal = other.contacts[0].normal;
 
         Quaternion portalRotation = Quaternion.LookRotation(contactNormal, Vector3.up);
-        GameObject spawnedPortal = Instantiate(portalPrefab, collisionPoint - forward.normalized * 1.01f, portalRotation);
-
+        //GameObject spawnedPortal = Instantiate(portalPrefab, collisionPoint - forward.normalized * 1.01f, portalRotation);
+        portalPrefab.transform.position = collisionPoint - forward.normalized * 1.01f;
+        portalPrefab.transform.rotation = portalRotation;
+        PortalMechanism pm = portalPrefab.GetComponent<PortalMechanism>();
+        pm.otherPortal = this.otherPortal;
+        portalPrefab.SetActive(true);
         //Destroy(this.gameObject);
         //instead of destroying.. disable it..
         //TODO: same as PlayerGunController
---
+++ Assets/singlePlayerMovement.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class singlePlayerMovement : MonoBehaviour {
+
+    public GameObject portalType;
+    public GameObject portalBluePrefab;
+    public GameObject portalRedPrefab;
+    public GameObject portalBlue;
+    public GameObject portalRed;
+    public Transform playerTransform;
+    public Rigidbody rb;
+    public GameObject portalbullet;
+    private PortalBulletController pbc;
+	// Use this for initialization
+	void Start () {
+        portalBlue = Instantiate(portalBlue);
+        portalBlue.SetActive(false);
+        portalRed = Instantiate(portalRed);
+        portalRed.SetActive(false);
+        portalbullet = Instantiate(portalbullet);
+        portalbullet.SetActive(false);
+        portalbullet2 = Instantiate(portalbullet);
+        portalbullet2.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.forward * 10);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(-transform.forward * 10);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-transform.right*10);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right*10);
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.up*10);
+        }
+        if(Input.GetKey(KeyCode.Q))
+        {
+            // portalType = portalBlue;
+            // GameObject g = Instantiate(portalbullet, playerTransform.position - playerTransform.right*5, Quaternion.identity);
+            portalbullet.SetActive(false);
+            portalbullet.transform.position = playerTransform.position + playerTransform.forward * 2;
+            portalbullet.transform.rotation = Quaternion.identity;
+            portalbullet.transform.forward = playerTransform.forward;
+            portalbullet.SetActive(true);
+            pbc = portalbullet.GetComponent<PortalBulletController>();
+            pbc.portalPrefab = portalBlue;
+            pbc.otherPortal = portalRed;
+        }
+        if(Input.GetKey(KeyCode.E))
+        {
+            //portalType = portalRed;
+            //GameObject g = Instantiate(portalbullet, playerTransform.position + playerTransform.right * 5, Quaternion.identity);
+            portalbullet2.SetActive(false);
+            portalbullet2.transform.position = playerTransform.position + playerTransform.forward * 2;
+            Debug.Log("position");
+            Debug.Log(portalbullet2.transform.position);
+            portalbullet2.transform.rotation = Quaternion.identity;
+            portalbullet2.transform.forward = playerTransform.forward;
+            Debug.Log("portal");
+            Debug.Log(portalbullet2.transform.forward);
+            portalbullet2.SetActive(true);
+            pbc = portalbullet2.GetComponent<PortalBulletController>();
+            pbc.portalPrefab = portalRed;
+            pbc.otherPortal = portalBlue;
+        }
+	}
+}
